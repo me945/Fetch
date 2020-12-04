2a1ade36dd5ef18aa538cb48d4800954b80528e2
me945 <me945@uowmail.edu.au>
2020-12-04 10:44:09 +0400

    get project owner

--- githubthingy.js
+++ githubthingy.js
@@ -1,20 +1,11 @@
 const path = require ("path");
 const http = require("https");
 var express = require("express");
-const { response } = require("express");
+const { response, json } = require("express");
 const app = express();
 const PORT = 3000;
 
-
-//"start":"nodemon github.js",
-let github = 
-[
-    {"username" : "user0","project": {"title": "sigh","description" : "its a project ","commits": 3}},
-    {"username" : "user1","project": {"title": "Sike3","description" : "its a project2 ","commits": 3}},
-    {"username" : "user2","project": {"title": "Sike3","description" : "its a project2 ","commits": 3}},
-    {"username" : "user2","project": {"title": "oblivious"},"description" : "its dark in here ","commits": 4},
-    {"username" : "user3","project": {"title": "space"},"description" : "i can't breath","commits": 5},
-]
+const data = require('./data.json');
 
 app.use(express.json());
 
@@ -25,39 +16,156 @@ app.get('/',(req, res)=>{
     res.send('Hello');
 })    
 
-app.get('/github', (req, res) => {
-    res.json(github);
+app.get('/data', (req, res) => {
+    res.json(data);
   });
 
-app.get('/github/:username', (req, res) => {
-    const user = String(req.params.username);
-    const getUser = github.filter((username) => username.username === user);
-  
+//function to find the user
+function checkForUser(user){
+  // console.log("this is user");
+  // console.log(user);
+     for (var i =0; i < data[0].user.length; i++){
+        if(data[0].user[i].id == user){
+        return(JSON.stringify(data[0].user[i]));
+      }
+     }
+}
+//function to find project owner
+function checkForProjectOwner(project){
+     for (var i =0; i < data[1].project.length; i++){
+        if(data[1].project[i].id == project){
+          var x =  JSON.stringify(data[1].project[i].Owner_id)
+      }
+     }
+
+     for(var j =0; j<data[0].user.length; j++){
+        if(data[0].user[j].id == x){
+           var userInfo = data[0].user[j];
+        }
+     }
+     return(userInfo);
+}
+
+
+//function to find project contributers
+function checkForProjectCon(project){
+
+     for (var i =0; i < data[2].Contributing.length; i++){
+        if(data[2].Contributing[i].Project_id == project){
+        return(JSON.stringify(data[0].user[i]));
+      }
+     }
+}
+
+//return user's info
+app.get('/data/:user', (req, res) => {
+    const user = String(req.params.user);
+    var getUser = checkForUser(user);
+
     if (!getUser) {
       res.status(500).send('User not found.')
     } else {
+      const h = JSON.stringify(getUser);
       res.json(getUser);
     }
   });
 
-
-  app.get('/github/:username/:project', (req, res) => {
+  //returns Porject that user owns
+  app.get('/data/:user/:project', (req, res) => {
     const project = String(req.params.project);
-    const user = String(req.params.username);
-    const getUser = github.find((x) => x.username === user && x.project.title === project);
+    const user = String(req.params.user);
+    const getProject = checkForProjectOwner(project);
 
-    if (!getUser) {
+    if (!getProject) {
       res.status(500).send('project not found.')
     } else {
-      res.json(getUser);
+      res.json(getProject);
     }
   });
 
+//  //returns project contibute
+//   app.get('/data/con/:project', (req, res) => {
+//     const project = String(req.params.project);
+//     // const user = String(req.params.user);
+//     const getUser = checkForProjectCon(project);
 
-const name = "Sike3";
-const arr = github.filter(x => x.project.title === name);
-console.log(arr);
+//     if (!getUser) {
+//       res.status(500).send('project not found.')
+//     } else {
+//       res.json(getUser);
+//     }
+//   });
 
+
+//const name = "Sike3";
+//const arr = github.filter(x => x.project.title === name);
+//const h = JSON.stringify (arr);
+//console.log(h);
+//console.log(arr);
+
+const someData = JSON.stringify(data,null,4);
+console.log(data);
+console.log(someData);
+
+
+const anyAdult = data.some(p => p.user[0].id === 1);
+console.log(anyAdult); // true
+
+//for loop for fetch user
+for(var i =0; i < data[0].user.length; i++){
+  if(data[0].user[i].id === 2){
+    console.log("Hello"+ JSON.stringify(data[0].user[i]));
+  }
+}
+
+//for loop to fetch porject
+for(var j=0; j < data[1].project.length; j++){
+
+  if(data[1].project[j].title === "pro2"){
+    console.log(data[1].project[j]);
+  }
+  
+}
+
+
+
+// console.log("Accessing the object");
+// const data2 = someData;
+//console.log(data2);
+
+// var n = 1;
+// const m = data.filter(x => x.user[1] .id === 1);
+
+  const u = data[0].user[0].id;
+  const p = data[1].project[1].title;
+  const c = data[2].Contributing[0].User_id;
+
+  console.log(u);
+  console.log(p);
+  console.log(c);
+
+  if (u === p ){
+    console.log("i found it");
+  }
+  const o = data[0].user[1].id;
+  console.log(o);
+
+//console.log(m);
+ const cat = {
+  code: 42,
+  items: [{
+      id: 1,
+      name: 'foo'
+  }, {
+      id: 2,
+      name: 'bar'
+  }]
+};
+
+console.log(cat);
+const item_name = cat.items[1].name;
+console.log(item_name);
+//console.log(github);
 // const users = JSON.stringify(github)
 // const objs = JSON.parse(users);
 // const trys = github.username = "user0";

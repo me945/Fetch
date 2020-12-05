4cc3c0d495faed5aaf6d0a35557c7b25ac4b1092
me945 <me945@uowmail.edu.au>
2020-12-05 12:15:32 +0400

    made new functions

--- githubthingy.js
+++ githubthingy.js
@@ -4,8 +4,8 @@ var express = require("express");
 const { response, json } = require("express");
 const app = express();
 const PORT = 3000;
-
 const data = require('./data.json');
+const { Console } = require("console");
 
 app.use(express.json());
 
@@ -24,8 +24,8 @@ app.get('/data', (req, res) => {
 function checkForUser(user){
      for (var i =0; i < data[0].user.length; i++){
         if(data[0].user[i].id == user){
-        return(JSON.stringify(data[0].user[i]));
-      }
+          return(JSON.stringify(data[0].user[i]));
+        }
      }
 }
 
@@ -34,7 +34,7 @@ function checkForProjectOwner(project){
      for (var i =0; i < data[1].project.length; i++){
         if(data[1].project[i].id == project){
           var x =  JSON.stringify(data[1].project[i].Owner_id)
-      }
+        }
      }
 
      for(var j =0; j<data[0].user.length; j++){
@@ -47,13 +47,20 @@ function checkForProjectOwner(project){
 
 //function to find project contributer
 function checkForProjectCon(project){
-
      for (var i =0; i < data[2].Contributing.length; i++){
         if(data[2].Contributing[i].Project_id == project){
-        return(JSON.stringify(data[0].user[i]));
-      }
+           var y = data[2].Contributing[i].User_id;
+        }
      }
+
+      for(var j =0; j< data[0].user.length; j++){
+        if(data[0].user[j].id == y){
+           var userInfo = data[0].user[j];
+        }
+     }
+     return(userInfo);
 }
+  
 
 //return user's info
 app.get('/data/:user', (req, res) => {
@@ -63,9 +70,9 @@ app.get('/data/:user', (req, res) => {
     if (!getUser) {
       res.status(500).send('User not found.')
     } else {
-      const h = JSON.stringify(getUser);
-      res.json(getUser);
-    }
+        const h = JSON.stringify(getUser);
+        res.json(getUser);
+      }
   });
 
   //returns Porject that user owns
@@ -81,104 +88,15 @@ app.get('/data/:user', (req, res) => {
     }
   });
 
-//---------------ignore below-------------------
+   //returns Porject contributers
+   app.get('/data/:user/:project/con', (req, res) => {
+    const project = String(req.params.project);
+    const user = String(req.params.user);
+    const getProject = checkForProjectCon(project);
 
-//  //returns project contibute
-//   app.get('/data/con/:project', (req, res) => {
-//     const project = String(req.params.project);
-//     // const user = String(req.params.user);
-//     const getUser = checkForProjectCon(project);
-
-//     if (!getUser) {
-//       res.status(500).send('project not found.')
-//     } else {
-//       res.json(getUser);
-//     }
-//   });
-
-
-//const name = "Sike3";
-//const arr = github.filter(x => x.project.title === name);
-//const h = JSON.stringify (arr);
-//console.log(h);
-//console.log(arr);
-
-const someData = JSON.stringify(data,null,4);
-console.log(data);
-console.log(someData);
-
-
-const anyAdult = data.some(p => p.user[0].id === 1);
-console.log(anyAdult); // true
-
-//for loop for fetch user
-for(var i =0; i < data[0].user.length; i++){
-  if(data[0].user[i].id === 2){
-    console.log("Hello"+ JSON.stringify(data[0].user[i]));
-  }
-}
-
-//for loop to fetch porject
-for(var j=0; j < data[1].project.length; j++){
-
-  if(data[1].project[j].title === "pro2"){
-    console.log(data[1].project[j]);
-  }
-  
-}
-
-// console.log("Accessing the object");
-// const data2 = someData;
-//console.log(data2);
-
-// var n = 1;
-// const m = data.filter(x => x.user[1] .id === 1);
-
-  const u = data[0].user[0].id;
-  const p = data[1].project[1].title;
-  const c = data[2].Contributing[0].User_id;
-
-  console.log(u);
-  console.log(p);
-  console.log(c);
-
-  if (u === p ){
-    console.log("i found it");
-  }
-  const o = data[0].user[1].id;
-  console.log(o);
-
-//console.log(m);
- const cat = {
-  code: 42,
-  items: [{
-      id: 1,
-      name: 'foo'
-  }, {
-      id: 2,
-      name: 'bar'
-  }]
-};
-
-console.log(cat);
-const item_name = cat.items[1].name;
-console.log(item_name);
-//console.log(github);
-// const users = JSON.stringify(github)
-// const objs = JSON.parse(users);
-// const trys = github.username = "user0";
-
-// console.log(trys);
-// console.log(github);
-// const json = '{ "fruit": "pineapple", "fingers": 10 }';
-// const obj = JSON.parse(json);
-// console.log(obj.fruit, obj.fingers);
-// const initialState = [
-//   {id:1 ,name: 'Product A', image: 'pic-001.jpg', tags: ['nature', 'camping', 'outdoor']},
-//   {id:2 ,name: 'Product B', image: 'pic-002.jpg', tags: ['winter', 'hiking', 'camping', 'snow']},
-//   {id:3 ,name: 'Product C', image: 'pic-003.jpg', tags: ['vacation', 'family', 'kids', 'river', 'lake', 'fishing']}
-// ]
-
-// console.log(data[0].user[1]);
-// console.log(initialState);
-
+    if (!getProject) {
+      res.status(500).send('project not found.')
+    } else {
+      res.json(getProject);
+    }
+  });

df57aaf9cf208b37cd22d4c61390313ceb82277e
me945 <me945@uowmail.edu.au>
2020-12-10 16:09:13 +0400

    fixed projuct function

--- githubthingy.js
+++ githubthingy.js
@@ -1,12 +1,14 @@
 const path = require ("path");
 const http = require("https");
-var express = require("express");
-const { response, json } = require("express");
-const app = express();
-const PORT = 3000;
-const data = require('./data.json');
-const { Console } = require("console");
 const fs = require('fs')
+const { response, json } = require("express");
+const { Console } = require("console");
+const {users, projects, contributes_on} = require('./data.js');
+//const data = require('./data');
+var express = require("express");
+const app = express();
+
+const PORT = 3000;
 
 app.use(express.json());
 
@@ -16,48 +18,50 @@ app.listen(PORT, () => {
 app.get('/',(req, res)=>{
     res.send('Hello');
 })    
-
 app.get('/data', (req, res) => {
     res.json(data);
   });
-
   
 //function to find the user
 function checkForUser(user){
-     for (var i =0; i < data[0].user.length; i++){
-        if(data[0].user[i].id == user){
-          return(JSON.stringify(data[0].user[i]));
+     for (var i =0; i < data.user.length; i++){
+        if(data.users[i].id == user){
+          return(JSON.stringify(data.user[i]));
         }
      }
 }
 
 //function to find project owner
-function checkForProjectOwner(project){
-     for (var i =0; i < data[1].project.length; i++){
-        if(data[1].project[i].id == project){
-          var x =  JSON.stringify(data[1].project[i].Owner_id)
+function checkForProjectOwner(user,project){
+     for (var i =0; i < projects.length; i++){
+        if(projects[i].id == project){
+          var x =  JSON.stringify(projects[i].Owner_id)
         }
      }
-
-     for(var j =0; j<data[0].user.length; j++){
-        if(data[0].user[j].id == x){
-           var userInfo = data[0].user[j];
-        }
-     }
+    
+     if( x == user){
+      for(var j =0; j<users.length; j++){
+          if(users[j].id == x){
+            var userInfo = users[j];
+          }
+      }
      return(userInfo);
+    }else {
+      return false;
+    }
 }
 
 //function to find project contributer
 function checkForProjectCon(project){
-     for (var i =0; i < data[2].Contributing.length; i++){
-        if(data[2].Contributing[i].Project_id == project){
-           var y = data[2].Contributing[i].User_id;
+     for (var i =0; i < data.Contributing.length; i++){
+        if(contributes_on[i].Project_id == project){
+           var y = contributes_on[i].User_id;
         }
      }
 
       for(var j =0; j< data[0].user.length; j++){
-        if(data[0].user[j].id == y){
-           var userInfo = data[0].user[j];
+        if(users[j].id == y){
+           var userInfo = users[j];
         }
      }
      return(userInfo);
@@ -65,8 +69,8 @@ function checkForProjectCon(project){
   
 
 //return user's info
-app.get('/data/:user', (req, res) => {
-    const user = String(req.params.user);
+app.get('/data/:userId', (req, res) => {
+    const user = String(req.params.userId);
     var getUser = checkForUser(user);
 
     if (!getUser) {
@@ -94,7 +98,7 @@ app.get('/data/:user', (req, res) => {
    app.get('/data/:user/:project/con', (req, res) => {
     const project = String(req.params.project);
     const user = String(req.params.user);
-    const getProject = checkForProjectCon(project);
+    const getProject = checkForProjectCon(user,project);
 
     if (!getProject) {
       res.status(500).send('project not found.')
@@ -103,12 +107,13 @@ app.get('/data/:user', (req, res) => {
     }
   });
 
-  fs.stat('data.json', (err, stats) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    console.log(stats)
-  })
+  // some stats
+  // fs.stat('data.json', (err, stats) => {
+  //   if (err) {
+  //     console.error(err)
+  //     return
+  //   }  
+  //   console.log(stats)
+  // })
 
   console.log(global)

e8f344377343d7ca0b711b0596ac305ef68190c3
me945 <me945@uowmail.edu.au>
2020-12-10 13:59:04 +0400

    initial commit

---
+++ data.js
@@ -0,0 +1,27 @@
+module.exports = {
+ 
+    "users":[{ "id" :1,
+              "name": "whatever"  
+            },
+
+            { "id" :2,
+               "name": "yes"  
+            }],
+
+    "projects":[{ "id" : 1,
+                 "title": "pro2",
+                 "Owner_id": 2
+               },
+
+               { "id" :2,
+                "title": "Pro2",
+                "Owner_id": 1
+              }],
+
+    "contributes_on":[{ "User_id": 1,
+                       "Project_id": 1  
+                    },
+                    { "User_id": 2,
+                      "Project_id": 2  
+                    }]
+  };

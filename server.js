aed70fa835e29d676353d0f92f4a166ea27a23a1
me945 <me945@uowmail.edu.au>
2020-12-22 00:37:39 +0400

    optmize the functions

--- server.js
+++ server.js
@@ -1,88 +1,77 @@
-const path = require('path')
-const http = require('https')
-const fs = require('fs')
-const { response, json } = require('express')
-const { Console } = require('console')
 const { users, projects, contributes_on } = require('./project')
-//const data = require('./data');
 var express = require('express')
-const project = require('./project')
-const { Server } = require('http')
-const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants')
 const app = express()
-
 const PORT = 3000
 
-app.use(express.json())
+app.set('json spaces', 4)
 
 app.listen(PORT, () => {
     console.log('Server running at: http://localhost:' + PORT)
 })
 
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.redirect('/projects/first')
 })
+
 app.get('/project', (req, res) => {
     res.json(projects)
 })
 
-//------------Find User Info---------------------------------------
+//---------Find the projects the user is coontibuting to-----------------------------------------
 
-//find user info
-function checkForUser(user) {
+//Object Example:  {
+//                  id: 2
+//                  Username : name,
+//                  projects: [{"id" : 1,
+//                  "title_name":"first_project",
+//                  "url" : /projects/name/first_project
+//                   },
+//
+//                   {"id" : 2,
+//                    "title_name":"second_project",
+//                    "url" : /projects/name/second_project
+//                   }]
+//                 }
+
+function userProjects(username) {
+    let user = {}
+    user.projects = []
+
+    //get username
     for (var i = 0; i < users.length; i++) {
-        if (users[i].id == user) {
-            return JSON.stringify(users[i])
+        if (users[i].name === username) {
+            // objtest.username = users[i].name
+            // userIdTest = users[i].id
+            user.username = users[i].name
+            user.id = users[i].id
         }
     }
-}
 
-//Object Example: { "id" :1, name": "whatever"}
-//return user's info
-app.get('/project/:userId', (req, res) => {
-    const user = String(req.params.userId)
-    var getUser = checkForUser(user)
-
-    if (!getUser) {
-        res.status(500).send('User not found.')
-    } else {
-        const h = JSON.parse(getUser)
-        res.json(getUser)
-    }
-})
-
-//-----------Find Project Owner---------------------------------------
-
-//find project owner
-function checkForProjectOwner(user, project) {
     for (var i = 0; i < projects.length; i++) {
-        if (projects[i].id === project) {
-            var x = JSON.stringify(projects[i].Owner_id)
+        if (projects[i].Owner_id === user.id) {
+            const project = {}
+            project.id = projects[i].id
+            project.title_name = projects[i].title_name
+            project.url = projects[i].url
+            user.projects.push(project)
         }
     }
-    if (x === user) {
-        for (var j = 0; j < users.length; j++) {
-            if (users[j].id === x) {
-                var userInfo = users[j]
-            }
-        }
-        return userInfo
-    } else {
-        return false
-    }
+
+    // objtest.projects = Object.assign(arrtest2)
+    return user
 }
 
-//Object Example: { "id" :2, name": "yes"}
-//return project owner
-app.get('/project/:project/:user', (req, res) => {
-    const project = String(req.params.project)
-    const user = String(req.params.user)
-    const getProject = checkForProjectOwner(user, project)
+// returns user projects that he/she contributed to
+app.get('/projects/:username', (req, res) => {
+    const username = req.params.username
+    const userInfo = userProjects(username)
 
-    if (!getProject) {
-        res.status(500).send('project not found.')
+    if (!userInfo) {
+        res.status(404).send('project not found.')
     } else {
-        res.json(getProject)
+        // res.setHeader('content-type', 'application/json')
+        //res.send(JSON.Stringfy(userInfo,null,4))
+        res.send(userInfo)
     }
 })
 
@@ -98,7 +87,7 @@ app.get('/project/:project/:user', (req, res) => {
 //                                    ]
 //                  },
 //Projetcs/username - > returns json object with project's info
-function fetchProjectInfo(user, project) {
+function fetchProjectInfo(username, projectTitle) {
     var obj = {},
         obj2 = {},
         arr = [],
@@ -106,8 +95,8 @@ function fetchProjectInfo(user, project) {
         p_id
 
     //adding project info
-    for (var i = 0; i < projects.length; i++) {
-        if (projects[i].title_name === project) {
+    for (let i = 0; i < projects.length; i++) {
+        if (projects[i].title_name === projectTitle) {
             obj.title_name = projects[i].title_name
             obj.description = projects[i].description
             obj.commits = projects[i].commits
@@ -116,104 +105,38 @@ function fetchProjectInfo(user, project) {
     }
 
     //find contirbutions
-    for (var i = 0; i < contributes_on.length; i++) {
-        if (contributes_on[i].id === p_id) {
-            arr.push(contributes_on[i].username)
+    for (let i = 0; i < contributes_on.length; i++) {
+        if (contributes_on[i].project_id === p_id) {
+            arr.push(contributes_on[i].user_id)
         }
     }
 
-    //get project info
-    for (var i = 0; i < arr.length; i++) {
-      for(var j=0; j<= users.length; j++){
-        if (users[j].username == arr[i]) {
-            obj2 = users[i]
-            delete obj2.id
-            arr2.push(obj2)
+    //get the username that belongs to that id
+    for (let i = 0; i < users.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (users[i].id === arr[j]) {
+                let obj = {}
+                obj.username = users[i].name
+                arr2.push(obj)
+            }
         }
-      }
     }
 
-    obj.contributers = Object.assign(arr2)
-    return JSON.stringify(obj, null, 4)
+    obj.contributers = arr2
+    return obj
 }
 
 //returns project titile info
-app.get('/projects/:user/:project_title', (req, res) => {
-    const project = String(req.params.project_title)
-    const user = String(req.params.user)
-    const getProject = fetchProjectInfo(user, project)
+app.get('/projects/:username/:project_title', (req, res) => {
+    const projectTitle = req.params.project_title
+    const username = req.params.username
+    const project = fetchProjectInfo(username, projectTitle)
 
-    if (!getProject) {
-        res.status(500).send('project not found.')
+    if (!project) {
+        res.status(404).send('Project not found.')
     } else {
-        res.json(getProject)
+        res.json(project)
     }
 })
 
-//---------Find the projects the user is coontibuting to-----------------------------------------
-
-//Object Example:  {
-//                  Username : name,
-//                  projects: [{"id" : 1,
-//                  "title_name":"first_project",
-//                  "url" : /projects/name/first_project
-//                   },
-//
-//                   {"id" : 2,
-//                    "title_name":"second_project",
-//                    "url" : /projects/name/second_project
-//                   }]
-//                 }
-
-function userProjects(user) {
-    //projects/:user/
-    var objtest = {},
-        proObj = {},
-        arrtest = [],
-        arrtest2 = []
-
-    //get username
-    for (var i = 0; i < users.length; i++) {
-        if (users[i].username === user) {
-            objtest.username = users[i].name
-            useridtest = users[i].id
-        }
-    }
-
-    //find contirbutions
-    for (var i = 0; i < contributes_on.length; i++) {
-        if (contributes_on[i].id === useridtest) {
-            arrtest.push(contributes_on[i].project_id)
-        }
-    }
-
-    //get project info
-    for (var i = 0; i < arrtest.length; i++) {
-        if (projects[i].id === arrtest[i]) {
-            proObj = projects[i]
-            delete proObj.Owner_id
-            delete proObj.description
-            delete proObj.commits
-            arrtest2.push(proObj)
-        }
-    }
-
-    objtest.projects = Object.assign(arrtest2)
-    return JSON.stringify(objtest, null, 4)
-}
-
-// returns user projects that he/she contributed to
-app.get('/projects/:user/', (req, res) => {
-    const user = String(req.params.user)
-    const getProject = userProjects(user)
-
-    if (!getProject) {
-        res.status(500).send('project not found.')
-    } else {
-        res.json(getProject)
-    }
-})
-
-
-
-
+console.log(contributes_on)

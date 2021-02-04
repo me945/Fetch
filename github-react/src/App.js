e43fa859035d2ab149e8b1a077af82af8d98583e
me945 <me945@uowmail.edu.au>
2021-02-04 19:06:53 +0400

    add route feature

--- github-react/src/App.js
+++ github-react/src/App.js
@@ -3,7 +3,7 @@ import Nav from './components/Nav/Nav.js'
 import Title from './components/Title/Title.js'
 import Profile from './components/Profile/Profile.js'
 import ProjectsList from './components/Project/ProjectsList.js'
-// import ProjectInfo from './components/ProjectInfo/ProjectsInfo.js'
+import ProjectInfo from './components/ProjectInfo/ProjectInfo.js'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
@@ -34,90 +34,56 @@ function App() {
         setUserName(name.target.value)
     }
 
+    //onClick Function
     const onClickHandler = async (e) => {
         e.preventDefault()
 
-        //Fetch user information from the server
-        const fetchUser = async () => {
-            const fetchData = await fetch(
-                `http://localhost:3000/projects/${username}`
-            )
-            const data = await fetchData.json()
+        const myURL = new URL(window.location.href)
+        myURL.pathname = 'userinfo'
+        myURL.search = `user=${username}`
 
-            console.log(data)
-            return data
-        }
-        //pass the values to the stuff
-        const getUserInfo = async () => {
-            const infoFromServer = await fetchUser()
-            setProjectInfo(infoFromServer.projects)
-            setUserInfo({
-                name: infoFromServer.name,
-                id: infoFromServer.id,
-            })
-        }
-        getUserInfo()
-    }
+        window.location.href = myURL
 
-    let x = 0
-    //pass username
-    const onPress = () => {
-        //check if the user entered a username
+        // //Fetch user information from the server
+        // const fetchUser = async () => {
+        //     const fetchData = await fetch(
+        //         `http://localhost:3000/projects/${username}`
+        //     )
+        //     const data = await fetchData.json()
 
-        // if (!a === f) {
-        //     alert('Please enter a username')
-        //     return
+        //     console.log(data)
+        //     return data
         // }
-        console.log(x)
-        x++
-        //pass the username to the fetch funtion
 
-        //change the url to ?user={username}
-    }
-
-    //button funtion
-    const getProjectName = (id) => {
-        console.log(id)
+        // //pass object values to the state
+        // const getUserInfo = async () => {
+        //     const infoFromServer = await fetchUser()
+        //     setProjectInfo(infoFromServer.projects)
+        //     setUserInfo({
+        //         name: infoFromServer.name,
+        //         id: infoFromServer.id,
+        //     })
+        // }
+        // getUserInfo()
     }
     return (
-        <div className="App">
-            <Nav />
-            <Title />
-            <div
-                style={{
-                    paddingBottom: 20,
-                    paddingTop: 20,
-                    textAlign: 'center',
-                }}
-            >
-                <div className="ui search">
-                    <div className="ui icon input">
-                        <i className="search icon"></i>
-                        <input
-                            className="prompt"
-                            placeholder="enter username here"
-                            type="text"
-                            value={username}
-                            onChange={onChangeHandler}
-                        ></input>
-                    </div>
-                    <button
-                        className=" ui primary button"
-                        type="submit"
-                        onClick={onClickHandler}
-                    >
-                        <i className="github icon"></i>
-                        Search
-                    </button>
-                </div>
+        <Router>
+            <div className="App">
+                <Nav
+                    onChange={onChangeHandler}
+                    onClick={onClickHandler}
+                    userName={username}
+                />
+                <Route path="/" component={Title} />
+                <Route path="/userinfo" component={Profile} />
+                <Route path="/projectinfo" component={ProjectInfo} />
+                {/* <Profile userinfo={userInfo} userName={username} /> */}
+                {/* <ProjectsList
+                    userProjects={projectInfo}
+                    getProjectName={getProjectName}
+                /> */}
             </div>
-            {/* <ProjectInfo userName={userURL} ProjectName={'projectName'} /> */}
-            <Profile userinfo={userInfo} userName={username} />
-            <ProjectsList
-                userProjects={projectInfo}
-                getProjectName={getProjectName}
-            />
-        </div>
+        </Router>
     )
 }
 export default App
